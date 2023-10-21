9fc9bc63f86f0c5f55861c6f3062eb34184fc6c9
rpjvanaert <ralf@van-aert.nl>
2023-10-21 09:53:24 +0200

    trigger pipeline #1

--- src/main/java/com/ralfvanaert/knightclubbing/Application.java
+++ src/main/java/com/ralfvanaert/knightclubbing/Application.java
@@ -11,7 +11,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("base-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hello!!!");
         stage.setScene(scene);
         stage.show();
     }
